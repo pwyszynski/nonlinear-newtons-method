f5016e1c31c293355a39d92fc6c733ae311d2622
pwyszynski <stuntmankg@gmail.com>
2014-12-14 16:44:05 +0100

    Added convergence check

--- lib/script.rb
+++ lib/script.rb
@@ -35,8 +35,6 @@ def GofX(startX, startY)
 	return g
 end
 
-
-
 #MAIN
 results = Array.new
 
@@ -47,6 +45,8 @@ puts "y0 = "
 firstY = gets.chomp.to_i
 puts "Podaj ilość iteracji"
 n = gets.chomp.to_i
+puts "Podaj ilość miejsc po przecinku (sugerowane max. 5):"
+precision = gets.chomp.to_i
 
 results[0] = GofX(firstX,firstY)
 
@@ -54,4 +54,42 @@ for i in (1...n) do
 	results[i] = GofX(results[i-1].element(0,0), results[i-1].element(1,0))
 end
 
-print results
+
+pomX = 0.0
+pomY = 0.0
+i = 0
+
+#drukowanie pierwszego elementu
+pomX = results[0].element(0,0).to_f.round(precision)
+pomY = results[0].element(1,0).to_f.round(precision)
+
+puts "x#{i} = " + "#{pomX}"
+puts "y#{i} = " + "#{pomY}"
+puts "\n"
+
+
+
+for i in (1...results.length) do
+	pomXp = results[i-1].element(0,0).to_f.round(precision)
+	pomYp = results[i-1].element(1,0).to_f.round(precision)
+
+	pomX = results[i].element(0,0).to_f.round(precision)
+	pomY = results[i].element(1,0).to_f.round(precision)
+
+	deltaX = pomX - pomXp
+	deltaX = deltaX.abs.round(2)
+	deltaY = pomY - pomYp
+	deltaY = deltaY.abs.round(2)
+
+	puts "x#{i} = " + "\t#{pomX}"
+	puts "y#{i} = " + "\t#{pomY}"
+	puts "DeX#{i} = \t#{deltaX}"
+	puts "DeY#{i} = \t#{deltaY}"
+	puts "\n"
+
+	if(deltaX == 0.0 || deltaY == 0)
+		puts "Rozwiązania tego układu równań są zbieżne."
+		break
+	end
+	
+end
